4c27e70b9513705dbf7d9dec995d2beeadd9d20c
DESKTOP-4TQN1EG\Eric <ecarlotto@intellipos.com.br>
2021-09-21 11:35:52 -0300

    Uploading the changes applied, about the second and third part of the course Laravel on Alura.com

---
+++ resources/views/series/index.blade.php
@@ -0,0 +1,16 @@
+@extends('layout')
+
+
+@section('cabecalho')
+    Series
+@endsection
+
+@section('conteudo')
+<a href="/series/create" class="btn btn-dark mb-2">Adicionar</a>
+
+<ul class="list-group">
+    @foreach($series as $serie)
+    <li class="list-group-item"><?= $serie; ?></li>
+    @endforeach
+</ul>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,6 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/series', 'SeriesController@index');
+Route::get('/series/create', 'SeriesController@create');
